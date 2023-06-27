e62c6f0d4ef95827fc6878c7d7efc860b1e7deb7
Daria N <smb_udk@vk.com>
2023-06-27 21:14:42 +0700

    Add files via upload

---
+++ lesson4/pack_backpack.py
@@ -0,0 +1,11 @@
+def pack_backpack(items, max_weight):
+    possible_items = []
+    for item, weight in items.items():
+        if weight <= max_weight:
+            possible_items.append(item)
+            max_weight -= weight
+    return possible_items
+
+items = {'tent': 5, 'water': 3, 'food': 4, 'clothes': 2, 'first aid kit': 1}
+max_weight = 10
+print(pack_backpack(items, max_weight)) 
